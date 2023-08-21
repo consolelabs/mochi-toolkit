af20197f86029d5cf6bc2a54fe87605f2c5e5076
trkhoi <ngotrongkhoi112@gmail.com>
2023-08-21 13:17:48 +0700

    chore: token formatter

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -1,9 +1,14 @@
 package formatter
 
 import (
+	"errors"
 	"fmt"
+	"math"
+	"math/big"
 	"strconv"
 	"strings"
+
+	"github.com/ethereum/go-ethereum/params"
 )
 
 func GetFirstDigitAfterDecimal(num float64) string {
@@ -45,4 +50,65 @@ func FormatTokenAmount(tokenAmount string, tokenDecimal int) string {
 	amount := ConvertBigIntString(tokenAmount, tokenDecimal)
 	amountFloat, _ := amount.Float64()
 	return FormatNumberDecimal(amountFloat)
+}
+
+func ConvertBigIntString(amount string, decimal int) *big.Float {
+	decimals := decimal
+	if decimal == 0 {
+		decimals = 18
+	}
+	wei := new(big.Int)
+	wei.SetString(amount, 10)
+	return WeiToEther(wei, int(decimals))
+}
+
+func WeiToEther(wei *big.Int, decimals ...int) *big.Float {
+	f := new(big.Float)
+	f.SetPrec(236) //  IEEE 754 octuple-precision binary floating-point format: binary256
+	f.SetMode(big.ToNearestEven)
+	fWei := new(big.Float)
+	fWei.SetPrec(236) //  IEEE 754 octuple-precision binary floating-point format: binary256
+	fWei.SetMode(big.ToNearestEven)
+
+	var e *big.Float
+	if len(decimals) == 0 {
+		e = big.NewFloat(params.Ether)
+	} else {
+		e = big.NewFloat(math.Pow(10, float64(decimals[0])))
+	}
+	return f.Quo(fWei.SetInt(wei), e)
+}
+
+// FloatToString convert float to big int string with given decimal
+// Ignore negative float
+// example: FloatToString("0.000000000000000001", 18) => "1"
+func FloatToString(s string, decimal int64) string {
+	c, _ := strconv.ParseFloat(s, 64)
+	if c < 0 {
+		return "0"
+	}
+	bigval := new(big.Float)
+	bigval.SetFloat64(c)
+
+	d := new(big.Float)
+	d.SetInt(big.NewInt(int64(math.Pow(10, float64(decimal)))))
+	bigval.Mul(bigval, d)
+
+	r := new(big.Int)
+	bigval.Int(r) // store converted number in r
+	return r.String()
+}
+
+// Cmp compare x and y and returns:
+//
+//	-1 if x <  y
+//	 0 if x == y
+//	+1 if x >  y
+func Cmp(x, y string) (int, error) {
+	n1, ok1 := new(big.Int).SetString(x, 10)
+	n2, ok2 := new(big.Int).SetString(y, 10)
+	if !ok1 || !ok2 {
+		return 0, errors.New("invalid x or y")
+	}
+	return n1.Cmp(n2), nil
 }
